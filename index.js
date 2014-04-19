e5012732b93a898da962d570bdd43b2dcbe3047d
pmorgan4801 <ppm3@comcast.net>
2014-04-19 13:33:17 -0400

    more particles

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ $(document).ready(function () {
 	var screenWidth = $(document).width();
     var screenHeight = $(document).height();
     
-    var hexSmall = '<img class="hexsmall" src="img/hex50.png">';
-    var hexMedium = '<img class="hexmedium" src="img/hex75.png">';
-    var hexLarge = '<img class="hexlarge" src="img/hex100.png">';
-    
     var hexSmallOb = document.getElementsByClassName("hexSmall");
     var hexMediumOb = document.getElementsByClassName("hexmedium");
     var hexLargeOb = document.getElementsByClassName("hexlarge");
@@ -17,14 +13,19 @@ $(document).ready(function () {
     var partDiv = document.getElementById("particles");
     
     function hexParticles() {
-        var spawn = Math.floor((Math.random() * screenWidth) + 1);
         var numb = Math.floor((Math.random() * 3) + 1);
         var hex = "";
         if (numb === 1) {
+            var spawn = Math.floor(((Math.random() * screenWidth) + 1) - (0.5 * $(".hexsmall").width()));
+            var hexSmall = '<img style="margin-left: ' + spawn.toString() + 'px;" class="hexsmall" src="img/hex50.png">';
             hex = hexSmall;
         } else if (numb === 2) {
+            var spawn = Math.floor(((Math.random() * screenWidth) + 1) - (0.5 * $(".hexmedium").width()));
+            var hexMedium = '<img style="margin-left: ' + spawn.toString() + 'px;" class="hexmedium" src="img/hex75.png">';
             hex = hexMedium;
         } else if (numb === 3) {
+            var spawn = Math.floor(((Math.random() * screenWidth) + 1) - (0.5 * $(".hexlarge").width()));
+            var hexLarge = '<img style="margin-left: ' + spawn.toString() + 'px;" class="hexlarge" src="img/hex100.png">';
             hex = hexLarge;
         }
         
@@ -32,5 +33,5 @@ $(document).ready(function () {
         
     }
     
-    window.setInterval(function () {hexParticles()}, 1000);
+    window.setInterval(function () {hexParticles()}, 100);
 });
