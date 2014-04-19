90ac931476774e1048821db80c0a9f4d565313b3
pmorgan4801 <ppm3@comcast.net>
2014-04-19 13:11:45 -0400

    added some functionality to particles

--- index.js
+++ index.js
@@ -2,7 +2,35 @@ $(document).ready(function () {
 	var screenWidth = $(document).width();
     var screenHeight = $(document).height();
     
+    var hexSmall = '<img class="hexsmall" src="img/hex50.png">';
+    var hexMedium = '<img class="hexmedium" src="img/hex75.png">';
+    var hexLarge = '<img class="hexlarge" src="img/hex100.png">';
+    
+    var hexSmallOb = document.getElementsByClassName("hexSmall");
+    var hexMediumOb = document.getElementsByClassName("hexmedium");
+    var hexLargeOb = document.getElementsByClassName("hexlarge");
+    
+    var hexSmallPlace = screenHeight - (0.5 * ($(".hexsmall").height()));
+    var hexMediumPlace = screenHeight - (0.5 * ($(".hexmedium").height()));
+    var hexLargePlace = screenHeight - (0.5 * ($(".hexlarge").height()));
+    
+    var partDiv = document.getElementById("particles");
+    
     function hexParticles() {
+        var spawn = Math.floor((Math.random() * screenWidth) + 1);
+        var numb = Math.floor((Math.random() * 3) + 1);
+        var hex = "";
+        if (numb === 1) {
+            hex = hexSmall;
+        } else if (numb === 2) {
+            hex = hexMedium;
+        } else if (numb === 3) {
+            hex = hexLarge;
+        }
+        
+        $("#particles").append(hex);
         
     }
+    
+    window.setInterval(function () {hexParticles()}, 1000);
 });
