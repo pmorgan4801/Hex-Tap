1857cbf9b8e81a6734d64a2197d8c702da28f9a5
pmorgan4801 <ppm3@comcast.net>
2014-04-19 15:33:41 -0400

    particles done

--- index.js
+++ index.js
@@ -5,36 +5,29 @@ $(document).ready(function () {
     $("#particles").css( "height", screenHeight.toString() + "px");
     $("#particles").css( "width", screenWidth.toString() + "px");
     
-    var hexSmallOb = document.getElementsByClassName("hexSmall");
-    var hexMediumOb = document.getElementsByClassName("hexmedium");
-    var hexLargeOb = document.getElementsByClassName("hexlarge");
-    
-    var hexSmallPlace = screenHeight - (0.5 * ($(".hexsmall").height()));
-    var hexMediumPlace = screenHeight - (0.5 * ($(".hexmedium").height()));
-    var hexLargePlace = screenHeight - (0.5 * ($(".hexlarge").height()));
-    
-    var partDiv = document.getElementById("particles");
-    
     function hexParticles() {
         var numb = Math.floor((Math.random() * 3) + 1);
         var hex = "";
         if (numb === 1) {
             var spawn = Math.floor(((Math.random() * screenWidth) + 1) - (0.5 * $(".hexsmall").width()));
-            var hexSmall = '<img style="margin-left: ' + spawn.toString() + 'px;" class="hexsmall" src="img/hex50.png">';
+            var hexSmallPlace = screenHeight - (0.5 * $(".hexsmall").height());
+            var hexSmall = '<img style="margin-left: ' + spawn.toString() + 'px; margin-top: ' + hexSmallPlace.toString() + 'px;" class="hexsmall" src="img/hex50.png">';
             hex = hexSmall;
         } else if (numb === 2) {
             var spawn = Math.floor(((Math.random() * screenWidth) + 1) - (0.5 * $(".hexmedium").width()));
-            var hexMedium = '<img style="margin-left: ' + spawn.toString() + 'px;" class="hexmedium" src="img/hex75.png">';
+            var hexMediumPlace = screenHeight - (0.5 * ($(".hexmedium").height()));
+            var hexMedium = '<img style="margin-left: ' + spawn.toString() + 'px; margin-top: ' + hexMediumPlace.toString() + 'px;" class="hexmedium" src="img/hex75.png">';
             hex = hexMedium;
         } else if (numb === 3) {
             var spawn = Math.floor(((Math.random() * screenWidth) + 1) - (0.5 * $(".hexlarge").width()));
-            var hexLarge = '<img style="margin-left: ' + spawn.toString() + 'px;" class="hexlarge" src="img/hex100.png">';
+            var hexLargePlace = screenHeight - (0.5 * ($(".hexlarge").height()));
+            var hexLarge = '<img style="margin-left: ' + spawn.toString() + 'px; margin-top: ' + hexLargePlace.toString() + 'px;" class="hexlarge" src="img/hex100.png">';
             hex = hexLarge;
         }
         
         $("#particles").append(hex);
-        
+        $("#particles img").animate({marginTop: "-=300px", opacity: "0.1"}, 2000, function () {$(this).hide()});
     }
     
-    window.setInterval(function () {hexParticles()}, 1000);
+    window.setInterval(function () {hexParticles()}, 100);
 });
